f85deb2ade79191f7f36d6fcb2761b2bb9428fac
Vignesh Raja <vigneshvrs5@gmail.com>
2024-10-31 10:35:10 -0400

    added analytics

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,24 @@
-var owl = $('.owl-carousel');
+var owl = $(".owl-carousel");
 owl.owlCarousel({
-    loop: false,
-    margin: 10,
-    nav: false,
-    dots: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 5
-        }
-    }
+  loop: false,
+  margin: 10,
+  nav: false,
+  dots: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
 });
 
-ScrollReveal().reveal('.landing', { delay: 200 });
-ScrollReveal().reveal('.about', { delay: 200 });
-ScrollReveal().reveal('.edu', { delay: 200 });
-ScrollReveal().reveal('.tech-stack', { delay: 200 });
-ScrollReveal().reveal('.projects', { delay: 200 });
+ScrollReveal().reveal(".landing", { delay: 200 });
+ScrollReveal().reveal(".about", { delay: 200 });
+ScrollReveal().reveal(".edu", { delay: 200 });
+ScrollReveal().reveal(".tech-stack", { delay: 200 });
+ScrollReveal().reveal(".projects", { delay: 200 });
